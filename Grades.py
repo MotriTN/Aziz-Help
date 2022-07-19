10609b6a839d19ddebaf48291b48edab81932361
MotriTN <gamerMT95@gmail.com>
2022-07-19 14:33:01 +0100

    initial

--- Grades.py
+++ Grades.py
@@ -6,13 +6,13 @@ print ("note devoir de controle:",dc)
 print ("note devoir de synthese:",ds)
 if M<8:
     print (M,"moyenne tres faible")
-elif 8<=M and M>10:
+elif 8<=M>10:
     print(M,"moyenne faible")
-elif 10<=M and M>12:
+elif 10<=M>12:
     print (M,"passable")
-elif 12<=M and M>14:
+elif 12<=M>14:
     print (M,"assez bien")
-elif 14<=M and M>16:
+elif 14<=M>16:
     print (M,"bien")
 else:
     print (m,"tres bien")
